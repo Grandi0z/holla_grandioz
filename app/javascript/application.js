024a6b37551a2f740008454eb43ef50134beeb4d
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-12-15 13:45:03 +0200

    build holla app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greetings from './components/Greetings'
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greetings />} />
+        <Route path="*" element={<p>Not found</p>} />
+      </Routes>
+    </BrowserRouter>
+  </>
+    );
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>,
+  </Provider>,
+  document.querySelector('#root'),
+);
---
+++ app/javascript/components/Greetings.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getGreetings } from '../src/redux/greetings/greetingsSlice';
+
+function Greetings() {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getGreetings())
+    }, [])
+    const greetings = useSelector((store) => store.greetings)
+  return (
+    <>{greetings.greetings.message}🖐️</>
+  );
+}
+
+export default Greetings;
